437449b7ceced9d9301109e511d77f175cecde85
Gustas2077 <333160@student.mboutrecht.nl>
2023-01-10 12:25:59 +0100

    Commit 2

--- mvc-oop-pdo-toets-userstory/app/views/includes/header.php
+++ mvc-oop-pdo-toets-userstory/app/views/includes/header.php
@@ -6,8 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= URLROOT; ?>/css/main.min.css">
-    <link rel="shortcut icon" href="<?= URLROOT; ?>/img/favicon.ico" type="image/x-icon">
-    <title>MVC Sandbox</title>
+    <title>MVC</title>
 </head>
 
 <body>
