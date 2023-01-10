ab98e1466203b0ce9685a3545d05795dd16fba55
Gustas2077 <333160@student.mboutrecht.nl>
2023-01-10 12:18:10 +0100

    first commit

---
+++ mvc-oop-pdo-toets-userstory/app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'be-toets-mvc');
+
+define('APPROOT', dirname(dirname(__FILE__)));
+
+define('URLROOT', 'http://mvc-oop-pdo-toets-userstory.com');
+
+define('SITENAME', 'MVC Framework Toets');
